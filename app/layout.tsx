e42472abc4eedddf1a69646fcd8058dec0f7ef2f
Zahrenshahh <zahrenshahh97@gmail.com>
2024-10-26 12:05:43 +0800

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,46 @@
-"use client";
-import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "../components/ui/navbar-menu";
-import { cn } from "../lib/utils";
 import './globals.css'
-import Link from "next/link";
+import React from "react";
+import { FloatingNav } from "../components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 
-
-
- 
-function Navbar({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+export function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
   return (
-    <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
-      <Menu setActive={setActive}>
-
-      
-      <Link href="/blog" className="text-black dark:text-white">Home</Link>
-
-        <MenuItem setActive={setActive} active={active} item="Services">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-            <ProductItem
-              title="Algochurn"
-              href=""
-              src=""
-              description="Prepare for tech interviews like never before."
-            />
-         
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
     </div>
   );
 }
+const DummyContent = () => {
+  return (
+    <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
+      <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
+        Scroll back up to reveal Navbar
+      </p>
+      <div className="inset-0 absolute bg-grid-black/[0.1] dark:bg-grid-white/[0.2]" />
+    </div>
+  );
+};
+
 
 
 
@@ -63,11 +53,40 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-       <Navbar/>
+        <FloatingNavDemo />
 
-          <main>{children}</main>
+        <main>{children}</main>
 
-        <footer>Footer</footer>
+        <footer>
+
+          <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+              <div className="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                </a>
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                  <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+              <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+            </div>
+          </footer>
+
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import { AnimatedPinDemo } from "../components/AnimatedPin";
+import { Slidingbar } from "../components/SlidingBar";
 
 export default function Home() {
   return (
     <div >
-     
-     <AnimatedPinDemo/>
+      <header className="grid grid-cols-2 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md ">
+
+        <div className="dark:text-white text-neutral-600 text-center text-4xl mt-20 font-bold">
+          <h3>Go Digital for
+            the 21st Century</h3>
+          <h1>Go Digital for
+            the 21st Century</h1>
+          <h2>UI/UX | Web Development |
+            Digital Marketing</h2>
+        </div>
+
+        <Slidingbar />
+
+      </header>
+      {/* <AnimatedPinDemo/> */}
     </div>
   );
 }
